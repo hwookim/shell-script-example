8fc5843c2874305d83bf9eba8160a635f340e230
Ubuntu <ubuntu@ip-192-168-0-110.ap-northeast-2.compute.internal>
2020-07-15 12:34:55 +0000

    feat: 2번 문제
     - 실행 중인 java 프로그램의 정보 표시

---
+++ running_java.sh
@@ -0,0 +1,12 @@
+PSINFO=`ps -eo pid,pcpu,pmem,vsz,rss,command | grep java | grep -v grep | awk '{print "pid : " $1, "\ncpu% : " $2, "\nmem% : " $3, "\nvsz : " $4, "\nrss : " $5}'`
+PORT=`lsof -i -P | grep java | awk '{print $9}'`
+LISTEN_CONNECTION=`netstat -anp | grep java | grep LISTEN | wc -l`
+ESTABLISHED_CONNECTION=`netstat -anp | grep java | grep ESTABLISHED | wc -l`
+
+{
+	echo -e "##running java program info\n"
+	echo "$PSINFO"
+  echo "PORT : $PORT"
+	echo "LISTEN CONNECTION : $LISTEN_CONNECTION"
+	echo "ESTABLISHED CONNECTION : $ESTABLISHED_CONNECTION"
+}
