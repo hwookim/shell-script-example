df9c79849f6694a963af06c220f1ed9cc9008318
HWoo_KIM <45786387+hwookim@users.noreply.github.com>
2020-07-19 23:36:44 +0900

    refactor: 실행 쉘 지정

--- running_java.sh
+++ running_java.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 PSINFO=`ps -eo pid,pcpu,pmem,vsz,rss,command | grep java | grep -v grep | awk '{print "pid : " $1, "\ncpu% : " $2, "\nmem% : " $3, "\nvsz : " $4, "\nrss : " $5}'`
 PORT=`lsof -i -P | grep java | awk '{print $9}'`
 LISTEN_CONNECTION=`netstat -anp | grep java | grep LISTEN | wc -l`
